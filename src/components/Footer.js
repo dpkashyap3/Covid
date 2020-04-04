1cec6f80827fd2dff2c7e080d56e171cc0b28284
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2020-04-04 13:19:41 +0530

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className="mt-5 bg-dark">
-            <h1 className="display-1">Deepak Kashyap</h1>
+        <div className="mt-5 bg-dark text-white">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <h1>Design And Developed By Deepak Kashyap</h1>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <h1>Database:https://covid19india.org</h1>
+                </div>
+            </div>
         </div>
     )
 }
