441ee6d1b2779c4a1b89c23163497b31073e2f72
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2020-04-04 13:07:51 +0530

    Updates

---
+++ precache-manifest.2da974083f4928a66a66215c988cfa4a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54be83c6d9e56d9b6368d610f9243d44",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "3e10f380c423504c5c78",
+    "url": "/covid/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a641d8744e007099f013",
+    "url": "/covid/static/css/main.dfb06f20.chunk.css"
+  },
+  {
+    "revision": "3e10f380c423504c5c78",
+    "url": "/covid/static/js/2.acfac727.chunk.js"
+  },
+  {
+    "revision": "b6ade95d779df7e640770412c1e70475",
+    "url": "/covid/static/js/2.acfac727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a641d8744e007099f013",
+    "url": "/covid/static/js/main.e242a0ff.chunk.js"
+  },
+  {
+    "revision": "2fe5251c04004cfae08b",
+    "url": "/covid/static/js/runtime-main.3561fde5.js"
+  },
+  {
+    "revision": "f503fc571968310d97894a998a9e6b23",
+    "url": "/covid/static/media/banner.f503fc57.png"
+  },
+  {
+    "revision": "1a6c4e92e1aa6624b8ccb4a71521d368",
+    "url": "/covid/static/media/logo.1a6c4e92.png"
+  }
+]);
